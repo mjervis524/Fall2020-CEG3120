9eb10a19bbb04f0ec16fa6a235f9aa72a4fcaf7b
pattonsgirl <pattonsgirl@gmail.com>
2020-09-30 19:28:00 +0000

    commit for problem breadboard

---
+++ scripts/demo.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# PATH requirement
+	# Check existence
+	# export script directory to PATH
+	# Append that line to .profile
+
+# Alias requirement
+	# Check existence
+	# create aliases
+	# append aliases to .bashrc
+
+# Vim plugin requirement
+	# pick plugin - download or keep folder in repo?
+	# Create / ammend .vimrc (vim needs to know about the plugins
+
+# MOTD requirement
+	# create a message
+	# delete unwanted messages
+
+# Help page requirement
+	# check if -help was an argument given
+
+# Something about functions
